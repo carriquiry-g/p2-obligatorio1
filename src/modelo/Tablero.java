7ee992647398b65d1d71e852474037d40a504ccb
carriquiry-g <carriquiry@ort.edu.uy>
2024-05-13 09:28:26 -0300

    actualizo fila y columna de auto al rotar tablero

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -282,10 +282,14 @@ public class Tablero {
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
                 Autito auto = configOriginal[i][j];
+                int nuevaFila = j;
+                int nuevaColumna = dim - 1 - i;
                 if (auto != null) {
                     auto.setDireccion(auto.rotar(1));
+                    auto.setFila(nuevaFila);
+                    auto.setColumna(nuevaColumna);
                 }
-                configRotada[j][dim - 1 - i] = auto;
+                configRotada[nuevaFila][nuevaColumna] = auto;
             }
         }
 
